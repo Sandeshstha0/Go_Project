d6cfd6150fd1b1c5ffc5642508c1f871dad0f1e8
Sandeshstha0 <sandeshstha519@gmail.com>
2026-09-06 08:53:42 +0545

    parse config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,31 +4,42 @@ import (
 	"flag"
 	"log"
 	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type HTTPServer struct {
 	Addr string `yaml:"addr" env:"ADDR" env-required:"true"`
 }
 
-// env_default:"production"
-
 type Config struct {
-	Env         string `yaml:"env" env:"ENV" env-required:"true" ` //struct tags
+	Env         string `yaml:"env" env:"ENV" env-default:"production"`
 	StoragePath string `yaml:"storage_path" env:"STORAGE_PATH" env-required:"true"`
 	HTTPServer  `yaml:"http_server"`
 }
 
-func MustLoad() {
-	var ConfigPath string
+func MustLoad() *Config {
+	var configPath string
 
-	ConfigPath = os.Getenv("CONFIG_PATH")
-	if ConfigPath == "" {
-		configPath := flag.String("config", "", "path to the configuration file")
+	configPath = os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		flag.StringVar(&configPath, "config", "", "path to the configuration file")
 		flag.Parse()
-		ConfigPath = *configPath
 	}
 
-	if ConfigPath == "" {
+	if configPath == "" {
 		log.Fatal("config path is not set: use CONFIG_PATH env var or --config flag")
 	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("cannot read config file: %s", err.Error())
+	}
+	return &cfg
 }
